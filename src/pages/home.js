eabcfbe4ce9baf249a101b1571bbd5744741d8a1
arriolajp <arriolajp7@gmail.com>
2024-08-13 11:39:21 -0400

    updated uri

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,7 +4,7 @@ import Axios from "axios";
 
 export function Home(){
   const CLIENT_ID="611973d560d1464fb5a4db10d6b19a78";
-  const REDIRECT_URI="https://master.d1djejxyh5xexr.amplifyapp.com";
+  const REDIRECT_URI="https://master.d1djejxyh5xexr.amplifyapp.com/callback";
   //host https://master.d1djejxyh5xexr.amplifyapp.com
   const AUTH_ENDPOINT="https://accounts.spotify.com/authorize";
   const RESPONSE_TYPE="token";
